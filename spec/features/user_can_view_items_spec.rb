942e5209cd9c2d84311f43d8a2cee3986fd7ab7f
Josh Thompson <thompsonjoshd@gmail.com>
2017-04-10 14:47:45 -0600

    create items

--- spec/features/user_can_view_items_spec.rb
+++ spec/features/user_can_view_items_spec.rb
@@ -4,9 +4,18 @@ RSpec.describe "as a visitor" do
 
   # Background: I have several items and each of them has a title, description, price, and image
   before :each do
-    Item.create()
-    Item.create()
-    Item.create()
+    Item.create(title: "Liver",
+                description: "Fresh livers from the healthiest patients",
+                price: 9999,
+                image: "https://ichef-1.bbci.co.uk/news/1024/media/images/73793000/jpg/_73793871_liver.jpg")
+    Item.create(title: "Small Intestine",
+                description: "Your intestine wearing out? Get a fresh one installed today!",
+                price: 7500,
+                image: "http://img.clipartall.com/illustration-of-a-small-small-intestine-clipart-1094_1300.jpg")
+    Item.create(title: "Heart",
+                description: "All of our hearts will eventually wear out. Why let yours?",
+                price: 25499,
+                image: "http://www.sciencemadesimple.co.uk/files/2016/10/sn-pigheart.jpg")
   end
 
   # title: Visitor can view items
@@ -18,9 +27,9 @@ RSpec.describe "as a visitor" do
       visit items_path
 
       # I can see all existing items
-      expect(page).to have_content()
-      expect(page).to have_content()
-      expect(page).to have_content()
+      expect(page).to have_content("Fresh livers from the healthiest patients")
+      expect(page).to have_content(9999)
+      expect(page).to have_css("img[src='https://ichef-1.bbci.co.uk/news/1024/media/images/73793000/jpg/_73793871_liver.jpg']")
     end
   end
 end
