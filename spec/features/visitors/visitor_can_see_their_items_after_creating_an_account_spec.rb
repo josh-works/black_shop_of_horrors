edc18447b9641070975e580aa422b46cfc01ba0f
Christopher Dale <christopher.john.dale3@gmail.com>
2017-04-12 18:18:41 -0600

    set up tests for story 8,
    three tests with visitor added

---
+++ spec/features/visitors/visitor_can_see_their_items_after_creating_an_account_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor can see items they added after becoming a 'user'" do
+  scenario 'they can see the items after becoming a registered user' do
+
+    visit items_path
+
+    click_on 'Add to Cart'
+
+    visit cart_path
+
+    expect(page).to have_link('Login or Create Account to Checkout')
+    # I feel this is too long of a name to have for a button, I think we
+    # should use a flash message to tell them they have to create an account
+    # to be able to checkout.
+    expect(page).not_to have_button("Checkout")
+  end
+end
