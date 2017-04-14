c2df63732ed72181c60160c0fc3e085f27d478df
Josh Thompson <thompsonjoshd@gmail.com>
2017-04-14 07:25:38 -0600

    refactor feature test, made more specific

--- spec/features/items/user_can_view_items_spec.rb
+++ spec/features/items/user_can_view_items_spec.rb
@@ -2,34 +2,31 @@ require 'rails_helper'
 
 RSpec.describe "as a visitor" do
 
-  # Background: I have several items and each of them has a title, description, price, and image
   before :each do
-    organs = Category.create(title: "Organs", image: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwivofOz-prTAhVLw2MKHaUHA_AQjRwIBw&url=http%3A%2F%2Fwww.shutterstock.com%2Fth%2Fpic-74337634%2Fstock-vector-internal-organs.html&psig=AFQjCNEyPRiWory-xw7aICgmJXUQg_zDkw&ust=1491950374950491")
+    organs = Category.create(title: "Organs", image: "https://www.google.com/image.png")
     organs.items.create(title: "Liver",
                 description: "Fresh livers from the healthiest patients",
                 price: 9999,
-                image: "https://ichef-1.bbci.co.uk/news/1024/media/images/73793000/jpg/_73793871_liver.jpg")
+                image: "https://ichef-1.bbci.co.uk/liver.jpg")
     organs.items.create(title: "Small Intestine",
                 description: "Your intestine wearing out? Get a fresh one installed today!",
                 price: 7500,
-                image: "http://img.clipartall.com/illustration-of-a-small-small-intestine-clipart-1094_1300.jpg")
+                image: "http://img.clipartall.com/i.jpg")
     organs.items.create(title: "Heart",
                 description: "All of our hearts will eventually wear out. Why let yours?",
                 price: 25499,
-                image: "http://www.sciencemadesimple.co.uk/files/2016/10/sn-pigheart.jpg")
+                image: "http://www.sciencemadesimple.co.ut.jpg")
   end
-  # title: Visitor can view items
+
   context "can view all items" do
     it "when on home page" do
-      # As a visitor
 
-      # When I visit "/items"
       visit items_path
-
-      # I can see all existing items
-      expect(page).to have_content("Fresh livers from the healthiest patients")
-      expect(page).to have_content(9999)
-      expect(page).to have_css("img[src='https://ichef-1.bbci.co.uk/news/1024/media/images/73793000/jpg/_73793871_liver.jpg']")
+      expect(page).to have_css('.items')
+      expect(page).to have_selector('.item_title', text: "Small Intestine")
+      expect(page).to have_selector('.item_title', count: 3)
+      expect(page).to have_selector('.price', text: /\$[,\d]+\.\d\d/)
+      expect(page).to have_css("img[src='https://ichef-1.bbci.co.uk/liver.jpg']")
     end
   end
 end
