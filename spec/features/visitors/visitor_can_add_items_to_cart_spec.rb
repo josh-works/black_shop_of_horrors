4f5371ce90c16eaba3a0a8b308c10b523ea2109f
Seth <seth19483@gmail.com>
2017-04-14 16:41:25 -0600

    some bugfixes, all tests passing

--- spec/features/visitors/visitor_can_add_items_to_cart_spec.rb
+++ spec/features/visitors/visitor_can_add_items_to_cart_spec.rb
@@ -1,17 +1,16 @@
 require 'rails_helper'
 
 RSpec.feature "Visitor can add items to the cart" do
-  xscenario "they cane see a cart page with items and the total price" do
+  scenario "they cane see a cart page with items and the total price" do
     @category = Category.create!(title: "nuclear weapons", image: "asdbfasdjfbaksdf")
     item = @category.items.create!(title: "Nuke", description: "Blow thine enemies to smitherines", price: 22000000, image: "https://www.ilovenukes.com")
 
-    visit categories_path
-    click_on 'Add'
+    visit category_path(@category)
+    click_on 'Add Nuke to Cart'
     click_on 'View Cart'
 
     expect(page).to have_current_path(cart_path)
     expect(page).to have_content(item.title)
-    expect(page).to have_css("img[src=\"#{item.image}\"]")
-    expect(page).to have_content("Total: $#{item.price}")
+    expect(page).to have_content("Total: #{item.format_price}")
   end
 end
