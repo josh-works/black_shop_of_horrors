2bf7e066108c598cea64f188016a2320bde2f692
Christopher Dale <christopher.john.dale3@gmail.com>
2017-04-17 14:33:26 -0600

    didn't change branches, updated and removed the organs table from the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170417201259) do
+ActiveRecord::Schema.define(version: 20170417202756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,11 +53,6 @@ ActiveRecord::Schema.define(version: 20170417201259) do
     t.datetime "updated_at"
     t.integer  "status",     default: 0
     t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
-  end
-
-  create_table "organs", force: :cascade do |t|
-    t.string  "name"
-    t.integer "price"
   end
 
   create_table "properties", force: :cascade do |t|
