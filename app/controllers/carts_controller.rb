0c9579e5fc5e8212f43976a5caf213ef71f6a5c6
Josh Thompson <thompsonjoshd@gmail.com>
2017-04-17 21:56:04 -0600

    refactor #items

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,7 +8,7 @@ class CartsController < ApplicationController
     session[:cart] = @cart.contents
 
     flash[:notice] = "You now have #{pluralize(@cart.count_of(item.id), item.title)}."
-    redirect_to :back
+    redirect_back(fallback_location: items_path)
   end
 
   def show
