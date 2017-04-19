2e7d9ebf9caedb444bd3d8bdb7d2b2f137735518
Josh Thompson <thompsonjoshd@gmail.com>
2017-04-19 15:52:12 -0600

    hotfix button name

--- spec/features/admin/admin_editing_an_item_spec.rb
+++ spec/features/admin/admin_editing_an_item_spec.rb
@@ -22,15 +22,9 @@ RSpec.feature "an admin visits /admin/items" do
     expect(current_path).to eq(item_path(@item))
     expect(page).to have_content("Joshs boring item")
     expect(page).to_not have_content("item1")
-
-    click_on "Back"
+  
+    click_on "Edit"
 
     expect(current_path).to eq(edit_admin_item_path(@item))
   end
 end
-
-# As an admin
-#  When I visit “admin/items”
-#  And I click “Edit”
-#  Then my current path should be “/admin/items/:ITEM_ID/edit”
-#  And I should be able to upate title, description, image, and status
